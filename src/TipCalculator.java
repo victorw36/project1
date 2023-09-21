b2bb8446a4715dfe2d33f38e8ad0e5aee9c02e89
BT_1N3_01 <sample@bths.edu>
2023-09-21 10:08:39 -0400

    SCANNER

---
+++ src/TipCalculator.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class TipCalculator {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Welcome to the Rong Restuarant. How many people will be eating: ");
+        int people = scan.nextInt();
+        scan.nextLine();
+
+
+    }
+}
