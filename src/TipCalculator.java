18ca60ec6a261fd912fe3738a6a90624dadbe80d
victorw36 <victorw36@nycstudents.net>
2023-10-01 23:49:06 -0400

    initial comment

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -11,16 +11,59 @@ public class TipCalculator {
         int tipPercent = scan.nextInt();
         scan.nextLine();
 
-        System.out.print("Enter a cost in dollars and cents (type -1 to end): ");
-        double number = scan.nextDouble();
-        scan.nextLine();
 
-        while ( number != -1) {
-            System.out.print("Enter a cost in dollars and cents (type -1 to end): ");
-            scan.nextLine();
+
+
+
+        double total = 0;
+
+        System.out.print("Enter a cost in dollars and cents (type done to end): ");
+
+        // loop for prices
+        while(scan.hasNextDouble()) {
+            System.out.print("Enter a cost in dollars and cents (type done to end): ");
+            double number =scan.nextDouble();
+            total = total + number;
         }
+//rounding to nearest 100ths
+        total = Math.round(total * 100.0) / 100.0;
 
+        double tipMoney = (tipPercent / 100.0 * total);
+        tipMoney = Math.round(tipMoney * 100.0) / 100.0;
+
+        double tipPerPerson = tipMoney / people;
+        tipPerPerson = Math.round(tipPerPerson * 100.0) / 100.0;
+
+        double totalPerPerson = total / people;
+        totalPerPerson = Math.round(totalPerPerson * 100.0) / 100.0;
+
+        double totalWithTip = tipMoney + total;
+        double realCostPerson = totalWithTip / people;
+        realCostPerson = Math.round(realCostPerson * 100.0) / 100.0;
+
+
+
+
+        //printing the products
         System.out.println("---------------------------------");
+        System.out.println("Total bill before tip: $" + total);
+        System.out.println("Total percentage is: " + tipPercent + "%");
+        System.out.println("Total tip: $" + tipMoney);
+        System.out.println("Total bill with tip: $" + totalWithTip);
+        System.out.println("Per person cost before tip: $" + totalPerPerson);
+        System.out.println("Tip per person: $" + tipPerPerson);
+        System.out.println("Total cost per person: $" + realCostPerson);
+        System.out.println("---------------------------------");
+
+
+        //freestyle, splitting bill by a different number
+
+
+
+
+
+
+
         scan.close();
 
         System.out.print("bye bye buddy");
